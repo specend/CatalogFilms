eba8a5501222a0ae94c483edd161a4b8d8ab2735
Геннадий Зябчук <gennaz2000@mail.ru>
2020-12-07 20:26:13 +0300

    A Controller is used to register an user

--- CatalogFilms/Controllers/AccountController.cs
+++ CatalogFilms/Controllers/AccountController.cs
@@ -9,6 +9,44 @@ namespace CatalogFilms.Controllers
     {
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                User user = new User { Email = model.Email, UserName = model.Email, FIO = model.FIO };
+                //add an user
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    //set up a cookie
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
         public IActionResult Index()
         {
             return View();
