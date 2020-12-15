badbb45224e84c41c1b490abac91c77c262a8014
Геннадий Зябчук <gennaz2000@mail.ru>
2020-12-15 19:59:29 +0300

    This is the migration of database because the new model сalled "Film" has been added

---
+++ CatalogFilms/Migrations/20201215164714_Initial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CatalogFilms.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Film",
+                columns: table => new
+                {
+                    Id_Film = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: false),
+                    Year = table.Column<int>(nullable: false),
+                    Director = table.Column<string>(nullable: false),
+                    Poster = table.Column<byte[]>(nullable: false),
+                    Id_User = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Film", x => x.Id_Film);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Film");
+        }
+    }
+}
